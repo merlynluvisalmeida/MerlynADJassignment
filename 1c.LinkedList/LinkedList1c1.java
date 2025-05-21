e6f645a36e419111b871571839208c8a5421248c
Merlyn Luvis Almeida <167005333+merlynluvisalmeida@users.noreply.github.com>
2025-05-21 21:56:19 +0530

    Update LinkedList1c1.java

--- 1c.LinkedList/LinkedList1c1.java
+++ 1c.LinkedList/LinkedList1c1.java
@@ -1,3 +1,10 @@
+/*
+Write a Java program to iterate through all elements in a linked list starting at the
+specified position ( 2nd) using iterator ( hint : Iterator iteratorobj = listobj.listIterator(1))
+
+*/
+
+
 package linkedList;
 import java.util.LinkedList;
 import java.util.Iterator;
