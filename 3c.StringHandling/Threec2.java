d40df2a689385c1333bc33390fdf2c341c714a8c
Merlyn Luvis Almeida <167005333+merlynluvisalmeida@users.noreply.github.com>
2025-05-21 22:53:04 +0530

    Update Threec2.java

--- 3c.StringHandling/Threec2.java
+++ 3c.StringHandling/Threec2.java
@@ -1,3 +1,8 @@
+/*
+Write a Java Program for Shortening a string to a specified length and adds an ellipsis using user
+defined function truncate()
+    */
+
 package twoa;
 import java.util.Scanner;
 public class Threec2 {
