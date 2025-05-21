00e06d4d962b9552c2a60f613424d44ec406248e
Merlyn Luvis Almeida <167005333+merlynluvisalmeida@users.noreply.github.com>
2025-05-21 22:00:15 +0530

    Update LinkedList1c5.java

--- 1c.LinkedList/LinkedList1c5.java
+++ 1c.LinkedList/LinkedList1c5.java
@@ -1,3 +1,9 @@
+/*
+Write a Java program that swaps two elements ( first and third elements ) in a linked list ( using
+Collections.swap(l_list, 0, 2))
+	*/
+
+
 package linkedList;
 import java.util.LinkedList;
 import java.util.Collections;
