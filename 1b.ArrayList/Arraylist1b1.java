05dacf33c30f0646e73eaa4bd7e102f944a79740
Merlyn Luvis Almeida <167005333+merlynluvisalmeida@users.noreply.github.com>
2025-05-21 21:38:19 +0530

    Update Arraylist1b1.java

--- 1b.ArrayList/Arraylist1b1.java
+++ 1b.ArrayList/Arraylist1b1.java
@@ -1,3 +1,8 @@
+/*
+Write a java program for getting different colors through ArrayList interface and search whether
+the color "Red" is available or not
+	*/
+
 package arrayList;
 import java.util.ArrayList;
 public class Arraylist1b1 {
