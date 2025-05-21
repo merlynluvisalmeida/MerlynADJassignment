e78ed88bb987177241b298ea3af4a2eddcaa4853
Merlyn Luvis Almeida <167005333+merlynluvisalmeida@users.noreply.github.com>
2025-05-21 22:54:49 +0530

    Update Threec5.java

--- 3c.StringHandling/Threec5.java
+++ 3c.StringHandling/Threec5.java
@@ -1,3 +1,9 @@
+/*
+Write a Java Program for Counting the number of words in a string using user defined function
+countWords()
+    */
+
+
 package twoa;
 import java.util.Scanner;
 public class Threec5 {
