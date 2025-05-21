08c13c9f4ac2e658142f424ae5769c5603ca446a
Merlyn Luvis Almeida <167005333+merlynluvisalmeida@users.noreply.github.com>
2025-05-21 22:45:06 +0530

    Update Threeb4.java

--- 3b.StringHandling/Threeb4.java
+++ 3b.StringHandling/Threeb4.java
@@ -1,3 +1,9 @@
+
+/*
+Q4. Write a Java Program for Checking if a string reads the same backward as forward (ignoring case
+and punctuation) using user defined function isPalindrome():
+    */
+
 package twoa;
 
 import java.util.Scanner;
