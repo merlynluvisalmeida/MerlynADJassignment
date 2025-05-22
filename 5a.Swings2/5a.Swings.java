eac736e8f3a8df475d4bd9dd133c960940b78b51
Merlyn Luvis Almeida <167005333+merlynluvisalmeida@users.noreply.github.com>
2025-05-22 19:13:13 +0530

    Update 5a.Swings.java

--- 5a.Swings2/5a.Swings.java
+++ 5a.Swings2/5a.Swings.java
@@ -5,32 +5,51 @@ display them on console whenever the countries are selected on the list.
 */
 
 
-package stringoperations;
-import javax.swing.*;
+ package Swings;
+import java.util.List;
+import javax.swing.DefaultListModel;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+public class JListExample extends JFrame {
+private JList<String> countryList;
+public JListExample() {
+//create the model and add elements
+DefaultListModel<String> listModel = new DefaultListModel<>();
+listModel.addElement("USA");
+listModel.addElement("India");
+listModel.addElement("Vietnam");
+listModel.addElement("Canada");
+listModel.addElement("Denmark");
+listModel.addElement("France");
+listModel.addElement("Great Britain");
+listModel.addElement("Japan");
+listModel.addElement("Africa");
+listModel.addElement("Greenland");
+listModel.addElement("Singapore");
+listModel.addElement("");
+//create the list
+countryList = new JList<>(listModel);
+countryList.addListSelectionListener(new ListSelectionListener() {
+@Override
+public void valueChanged(ListSelectionEvent e) {
+if (!e.getValueIsAdjusting()) {
+final List<String> selectedValuesList = countryList.getSelectedValuesList();
+System.out.println(selectedValuesList);
+}
+}
+});
+add(new JScrollPane(countryList));
+this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+this.setTitle("JList Example");
+this.setSize(200, 200);
+this.setLocationRelativeTo(null);
+this.setVisible(true);
 
-public class program5a {
-    public static void main(String[] args) {
-        // Array of country names
-        String[] countries = {
-            "USA", "India", "Vietnam", "Canada", "Denmark",
-            "France", "Great Britain", "Japan", "Africa",
-            "Greenland", "Singapore"
-        };
-
-        // Create the JFrame
-        JFrame frame = new JFrame("Country List");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300, 300);
-
-        // Create the JList and put it in a JScrollPane
-        JList<String> countryList = new JList<>(countries);
-        JScrollPane scrollPane = new JScrollPane(countryList);
-
-        // Add the scroll pane (which contains the JList) to the frame
-        frame.add(scrollPane);
-
-        // Center and display the frame
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-    }
+}
+public static void main(String[] args) {
+new JListExample();
+}
 }
